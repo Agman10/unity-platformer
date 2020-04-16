7fed0d48643b9c9bd1d43bbfe959a2a28ead991d
Agman10 <ag.man@live.se>
2020-04-16 11:25:06 +0200

    sorted files and added fly enemy

---
+++ Assets/enemies/FlyEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEnemy : EnemyLogic {
+    public float speed;
+    public bool up = true;
+    public float maxY;
+    public float minY;
+
+    void Update() {
+        if (alive)
+        {
+            Movement();
+            ChangeDir();
+        }
+    }
+    private void Movement()
+    {
+        if (up)
+        {
+            speed = 1f;
+        }
+        else if (!up)
+        {
+            speed = -1f;
+        }
+        rb2d.velocity = new Vector2(rb2d.velocity.x, +speed);
+        Debug.Log("moving");
+    }
+
+    private void ChangeDir()
+    {
+        if(trnsfrm.position.y >= maxY)
+        {
+            up = false;
+        }
+        if(trnsfrm.position.y <= minY)
+        {
+            up = true;
+        }
+    }
+}
